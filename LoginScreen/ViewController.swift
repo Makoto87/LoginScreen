9b6416892d76306e4938e266e7bf31e441c50bfe
Makoto87 <makosinhori@gmail.com>
2019-12-17 07:38:33 +0900

    Initial Commit

---
+++ LoginScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreen
+//
+//  Created by 堀田真 on 2019/12/17.
+//  Copyright © 2019 堀田真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
